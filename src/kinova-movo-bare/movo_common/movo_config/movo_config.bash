4f49c2ef9c0ee8e6239fc7e32ec2090f772ed3dd
ari <anders.ariel@gmail.com>
2019-02-04 16:33:37 -0500

    moveit

--- src/kinova-movo-bare/movo_common/movo_config/movo_config.bash
+++ src/kinova-movo-bare/movo_common/movo_config/movo_config.bash
@@ -85,7 +85,6 @@ export USE_R85_FOR_MOVEIT_CONFIG=false
 export MOVO_HAS_RIGHT_ROBOTIQ_GRIPPER=false # should be true
 export MOVO_HAS_LEFT_ROBOTIQ_GRIPPER=false
 
-export USE_MIXED_CONFIG_FOR_MOVEIT_CONFIG=false # should be true
 
 
 #XXX  MOVEIT HACK
@@ -93,10 +92,10 @@ export USE_KG3_FOR_MOVEIT_CONFIG=true
 export MOVO_HAS_RIGHT_KG3_GRIPPER=true
 export MOVO_HAS_LEFT_KG3_GRIPPER=true 
 
-#export USE_R85_FOR_MOVEIT_CONFIG=true
 #export MOVO_HAS_RIGHT_ROBOTIQ_GRIPPER=true
-#export MOVO_HAS_LEFT_ROBOTIQ_GRIPPER=true
-#export USE_MIXED_CONFIG_FOR_MOVEIT_CONFIG=false
+#export MOVO_HAS_LEFT_KG3_GRIPPER=true
+#export USE_MIXED_CONFIG_FOR_MOVEIT_CONFIG=true
+
 
 #export for kinect2 bridge
 export OCL_IGNORE_SELF_TEST=1
