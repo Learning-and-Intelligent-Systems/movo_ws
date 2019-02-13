3723dade447762e0d9ab12e79e220fc86c9833ea
sparrowcat <silvia.knappe725@gmail.com>
2019-02-13 16:35:09 -0500

    attempt to fix lag, rewrote as class also

--- src/lis_movo_pkg/scripts/keyboard_teleop.py
+++ src/lis_movo_pkg/scripts/keyboard_teleop.py
@@ -10,106 +10,111 @@ from geometry_msgs.msg import Twist
 from movo.system_defines import TRACTOR_REQUEST
 from movo_msgs.msg import ConfigCmd
 
-import sys, select, termios, tty
+import sys, select, termios, tty, threading
 
-msg = """
-Use the keyboard to move around!
---------------------------------
-        W
-    A       D
-        S
+class KeyboardTeleop:
+    def __init__(self):
+        self.msg = """
+            Use the keyboard to move around!
+            --------------------------------
+                    W
+                A       D
+                    S
 
-    to translate
-    ------------
+                to translate
+                ------------
 
-    Q       E
+                Q       E
 
-    to rotate
---------------------------------
-"""
+                to rotate
+            --------------------------------
+            """
 
-print msg
+        self.moves = {
+            'w':[1,0,0],
+            'a':[0,1,0],
+            's':[-1,0,0],
+            'd':[0,-1,0],
+            'q':[0,0,1],
+            'e':[0,0,-1]
+        }
 
-moves = {
-    'w':[1,0,0],
-    'a':[0,1,0],
-    's':[-1,0,0],
-    'd':[0,-1,0],
-    'q':[0,0,1],
-    'e':[0,0,-1]
-}
+        self.pub = rospy.Publisher('/movo/cmd_vel', Twist, queue_size=1)
+        self.cfg_cmd = ConfigCmd()
+        self.cfg_pub = rospy.Publisher('/movo/gp_command', ConfigCmd, queue_size=1)
+        self.r = rospy.Rate(10)
 
-def getKey():
-
-    fd = sys.stdin.fileno()
-    old = termios.tcgetattr(fd)
-
-    try:
-        tty.setraw(sys.stdin.fileno())
-        [i,o,e] = select.select([sys.stdin.fileno()],[],[],0.2)
-        if i:
-            ch = sys.stdin.read(1)
-        else:
-            ch = None
-    finally:
-        termios.tcsetattr(fd,termios.TCSADRAIN,old)
-
-    return ch
+        self.settings = termios.tcgetattr(sys.stdin)
 
 
-if __name__ == "__main__":
-    settings = termios.tcgetattr(sys.stdin)
+        self.v_x = 0
+        self.v_y = 0
+        self.a_z = 0
 
-    rospy.init_node('keyboard_teleop')
-    cfg_cmd = ConfigCmd()
-    cfg_pub = rospy.Publisher('/movo/gp_command', ConfigCmd, queue_size=1)
-    pub = rospy.Publisher('/movo/cmd_vel', Twist, queue_size=1)
+    def getKey(self):
 
-    v_x = 0
-    v_y = 0
-    a_z = 0
-    
-    try:
-        while(1):
+        fd = sys.stdin.fileno()
+        old = termios.tcgetattr(fd)
+
+        try:
+            tty.setraw(sys.stdin.fileno())
+            [i,o,e] = select.select([sys.stdin.fileno()],[],[],0.2)
+            if i:
+                ch = sys.stdin.read(1)
+            else:
+                ch = None
+        finally:
+            termios.tcsetattr(fd,termios.TCSADRAIN,old)
+
+        return ch
+
+    def setGP(self):
+        while not rospy.is_shutdown():
             cfg_cmd.gp_cmd = 'GENERAL_PURPOSE_CMD_SET_OPERATIONAL_MODE'
             cfg_cmd.gp_param = TRACTOR_REQUEST
             cfg_cmd.header.stamp = rospy.get_rostime()
-
             cfg_pub.publish(cfg_cmd)
+            r.sleep
 
-            twist = Twist()
+    def start(self):
+        rospy.init_node('keyboard_teleop')
 
-            key = getKey()
+        threading.Thread(target=setGP)
+        
+        while not rospy.is_shutdown():
+            try:
 
-            v_x = 0
-            v_y = 0
-            a_z = 0
+                twist = Twist()
 
-            if key in moves:
-                v_x = moves[key][0]
-                v_y = moves[key][1]
-                a_z = moves[key][2]
+                key = getKey()
 
-            else:
                 v_x = 0
                 v_y = 0
                 a_z = 0
-                if (key == '\x03'):
-                    break
-                    
-            twist.linear.x = v_x
-            twist.linear.y = v_y
-            twist.angular.z = a_z
-            pub.publish(twist)
 
-    except Exception as e:
-        print(e)
+                if key in moves:
+                    v_x = moves[key][0]
+                    v_y = moves[key][1]
+                    a_z = moves[key][2]
 
-    finally:
-        twist = Twist()
-        twist.linear.x = 0
-        twist.linear.y = 0
-        twist.angular.z = 0
-        pub.publish(twist)
+                else:
+                    v_x = 0
+                    v_y = 0
+                    a_z = 0
+                    if (key == '\x03'):
+                        break
+                        
+                twist.linear.x = v_x
+                twist.linear.y = v_y
+                twist.angular.z = a_z
+                pub.publish(twist)
 
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+                termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+
+            except Exception as e:
+                print(e)
+
+
+if __name__ == "__main__":
+    kt = KeyboardTeleop()
+    kt.start()
