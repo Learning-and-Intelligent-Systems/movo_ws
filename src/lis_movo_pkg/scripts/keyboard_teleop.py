770a686025367b59e75361c29324f5f522117a91
silvia.knappe725@gmail.com <anders.ariel@gmail.com>
2019-03-13 15:45:38 -0400

    now with torso control! (laggy_ )

--- src/lis_movo_pkg/scripts/keyboard_teleop.py
+++ src/lis_movo_pkg/scripts/keyboard_teleop.py
@@ -6,7 +6,8 @@ import rospy
 
 from geometry_msgs.msg import Twist
 from movo.system_defines import TRACTOR_REQUEST
-from movo_msgs.msg import ConfigCmd
+from movo_msgs.msg import ConfigCmd, LinearActuatorCmd
+from sensor_msgs.msg import JointState
 import sys, select, termios, tty, threading
 
 class KeyboardTeleop:
@@ -20,14 +21,14 @@ class KeyboardTeleop:
 
                 to translate
                 ------------
-
-                Q       E
-
-                to rotate
+                                    R
+                Q       E           
+                                    F
+                to rotate       to move torso
             --------------------------------
             """
 
-        self.moves = {
+        self.basemoves = {
             'w':[1,0,0],
             'a':[0,1,0],
             's':[-1,0,0],
@@ -36,17 +37,28 @@ class KeyboardTeleop:
             'e':[0,0,-1]
         }
 
-        self.pub = rospy.Publisher('/movo/cmd_vel', Twist, queue_size=1)
+        self.torsomoves = {
+            'r' : .002,
+            'f' : -.002
+        }
+
+        self.base_pub = rospy.Publisher('/movo/cmd_vel', Twist, queue_size=1)
         self.cfg_cmd = ConfigCmd()
         self.cfg_pub = rospy.Publisher('/movo/gp_command', ConfigCmd, queue_size=1)
+        self.torso_pub = rospy.Publisher('/movo/linear_actuator_cmd', LinearActuatorCmd, queue_size=1)
         self.r = rospy.Rate(10)
         self.thread = threading.Thread(target=self.setGP)
         self.thread_r = rospy.Rate(1)
         self.kill = False
-
+        self.current_pos = None
+        rospy.Subscriber('/movo/linear_actuator/joint_states', JointState, self.joint_state_cb)
 
         self.settings = termios.tcgetattr(sys.stdin)
 
+    def joint_state_cb(self, msg):
+        assert msg.name[0] == 'linear_joint'
+        if self.current_pos is None:
+            self.current_pos = msg.position[0]
 
     def getKey(self):
         fd = sys.stdin.fileno()
@@ -77,21 +89,29 @@ class KeyboardTeleop:
             try:
 
                 twist = Twist()
+                lincmd = LinearActuatorCmd()
 
                 key = self.getKey()
                 v_x = 0
                 v_y = 0
                 a_z = 0
+                torso_dz = 0
 
-                if key in self.moves:
-                    v_x = self.moves[key][0]
-                    v_y = self.moves[key][1]
-                    a_z = self.moves[key][2]
+                
 
+                if key in self.basemoves:
+                    v_x = self.basemoves[key][0]
+                    v_y = self.basemoves[key][1]
+                    a_z = self.basemoves[key][2]
+
+                elif key in self.torsomoves:
+                    torso_dz = self.torsomoves[key]
+                  
                 else:
                     v_x = 0
                     v_y = 0
                     a_z = 0
+                    torso_dz = 0
                     if (key == '\x03'):
                         print "Goodbye!"
                         self.kill = True
@@ -100,7 +120,14 @@ class KeyboardTeleop:
                 twist.linear.x = v_x
                 twist.linear.y = v_y
                 twist.angular.z = a_z
-                self.pub.publish(twist)
+                self.current_pos += torso_dz
+
+                lincmd.desired_position_m = self.current_pos                 
+                self.torso_pub.publish(lincmd)
+
+                self.base_pub.publish(twist)
+
+                self.current_pos = None
 
                 termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
 
